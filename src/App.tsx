3bc773756bd2726b26b99cdabb285ecf3228aaa0
Fabiano Santos Ramos <fabianosantosramos1978@gmail.com>
2025-04-24 07:24:57 -0300

    refactor components folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,13 @@
 import './styles/theme.css';
 import './styles/global.css';
-import { Heading } from './components/Heading';
-import { TimerIcon } from 'lucide-react';
+import { Container } from './components/Container';
 
 function App() {
   return (
     <>
-      <Heading title='Hello World'>
-        <button>
-          <TimerIcon />
-        </button>
-      </Heading>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed, quam
-        cupiditate? Tenetur dolore doloribus dolores cum? Fuga iste dicta
-        quidem? Eaque repellat error incidunt deleniti, est voluptatibus
-        expedita atque. Non.
-      </p>
+      <Container>
+        <h1>LOGO</h1>
+      </Container>
     </>
   );
 }

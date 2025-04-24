1fc1cac48bf28b5b26a7689936e4944b30e840ed
Fabiano Santos Ramos <fabianosantosramos1978@gmail.com>
2025-04-24 08:28:13 -0300

    add menu component

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -3,12 +3,12 @@ import styles from './styles.module.css';
 
 function Logo() {
   return (
-    <>
+    <div className={styles.logo}>
       <a href='#' className={styles.logoLink}>
         <TimerIcon />
         <span>Chronos</span>
       </a>
-    </>
+    </div>
   );
 }
 
